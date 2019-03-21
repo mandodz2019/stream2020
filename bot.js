ee3e9b8451724fdcca03dc0bbdfefbfcb88edd69
mandodz2019 <46611116+mandodz2019@users.noreply.github.com>
2019-03-21 13:58:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,7 @@ client.on('ready', () => {
 
 
 
-const adminprefix = "s";
+const adminprefix = "!";
 const developers = ["546374068346159104"];
 
 console.log("FOIL");
@@ -45,30 +45,30 @@ client.on('message', message => {//// حقوق @! FOIL
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
-  if (message.content.startsWith(adminprefix + 'a')) {
+  if (message.content.startsWith(adminprefix + 'pl')) {
     client.user.setGame(argresult);
       message.channel.send(`**:white_check_mark:  تم تغيير البلاينق الى   ${argresult}**`)
   } else 
-     if (message.content === (adminprefix + "b")) {
+     if (message.content === (adminprefix + "pl")) {
     message.guild.leave();        
   } else  
-  if (message.content.startsWith(adminprefix + 'z')) {
+  if (message.content.startsWith(adminprefix + 'wt')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
       message.channel.send(`**:white_check_mark:  تَم تغيير الواتشينق الى   ${argresult}**`)
   } else 
-  if (message.content.startsWith(adminprefix + 'e')) {
+  if (message.content.startsWith(adminprefix + 'ls')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
       message.channel.send(`**:white_check_mark:  تَم تغيير الليسينينق الى   ${argresult}**`)
   } else
-  if (message.content.startsWith(adminprefix + 'r')) {
+  if (message.content.startsWith(adminprefix + 'lf')) {
     client.user.setGame(argresult, "https://www.twitch.tv/paaaaaartly");
       message.channel.send(`**:white_check_mark:  تم تغييرك حالتك بالتويتش الى   ${argresult}**`)
   }
-  if (message.content.startsWith(adminprefix + 't')) {
+  if (message.content.startsWith(adminprefix + 'by')) {
   client.user.setUsername(argresult).then
       message.channel.send(`**:white_check_mark:  جاري تغيير الأسم لـ ..${argresult}** `)
 } else
-if (message.content.startsWith(adminprefix + 'y')) {
+if (message.content.startsWith(adminprefix + 'af')) {
   client.user.setAvatar(argresult);
     message.channel.send(`**:white_check_mark:  جاري تغيير الأفتار... :** `);
 }
